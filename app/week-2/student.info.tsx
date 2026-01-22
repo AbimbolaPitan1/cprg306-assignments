1b04c7bebf9a3041e88b2f91f80a859efcdb3656
AbimbolaPitan1 <toluabimbola.pitan@gmail.com>
2026-01-22 13:50:53 -0700

    inital commit of week 2 assignment

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Abimbola Pitan</h1>
+    <Link href="https://github.com/AbimbolaPitan1">my Github  repositories</Link>
+    </main>
+  );
+}
