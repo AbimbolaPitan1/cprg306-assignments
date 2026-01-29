b57ca5ab87d59ff81368b9fa03402c4fbac226ec
AbimbolaPitan1 <toluabimbola.pitan@gmail.com>
2026-01-29 14:25:18 -0700

    week 3

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <main>
+      <main className="max-w-md mx-auto bg=blue"> 
         <h1>CPRG 306: Web Development 2 Assignments</h1>
-        <p>
+
+        <p style={{ marginTop: "12px" }}>
           <Link href="/week-2">Go to week 2</Link>
+        </p>
+
+        <p>
+          <Link href="/week-3">Go to week 3</Link>
         </p>
       </main>
     </div>
-  )
-};
+  );
+}
+
