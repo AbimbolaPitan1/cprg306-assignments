09737996b1feabce89da1096c78744069c14a270
AbimbolaPitan1 <toluabimbola.pitan@gmail.com>
2026-03-12 14:12:19 -0600

    week 8

--- app/week-8/layout.tsx
+++ app/week-8/layout.tsx
@@ -1,9 +1,5 @@
 import { AuthContextProvider } from "./_utils/auth-context";
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return <AuthContextProvider>{children}</AuthContextProvider>;
 }
--- app/week-8/shopping-list/page.tsx
+++ app/week-8/shopping-list/page.tsx
@@ -1,26 +1,41 @@
 "use client";
 
-import { useState } from "react";
+import { useUserAuth } from "../_utils/auth-context";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 import ItemList from "./item-list";
 import MealIdeas from "./meal-ideas";
 import items from "./items.json";
 
 export default function Page() {
+  const { user } = useUserAuth();
+  const router = useRouter();
   const [selectedItem, setSelectedItem] = useState("");
 
+  useEffect(() => {
+    if (!user) {
+      router.replace("/week-8"); 
+    }
+  }, [user, router]);
+
+  if (!user) {
+    return null;
+  }
+
   const handleItemSelect = (item: any) => {
-    
     const cleanedName = item.name
-      .split(" ")[0]
-      .replace(",", "")
+      .split(",")[0]
+      .replace(/[^a-zA-Z ]/g, "")
+      .trim()
       .toLowerCase();
 
     setSelectedItem(cleanedName);
   };
 
   return (
-    <main className="p-4">
-      <h1 className="text-lg font-bold mb-3">Shopping List</h1>
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Shopping List</h1>
 
       <ItemList
         items={items}
