50318c13015dcd7ebe4913de9907626c3903270e
AbimbolaPitan1 <toluabimbola.pitan@gmail.com>
2026-03-25 12:29:55 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { AuthContextProvider } from "./week-10/_utils/auth-context";
 
 export const metadata: Metadata = {
   title: "Shopping List",
@@ -13,8 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AuthContextProvider>
+          {children}
+        </AuthContextProvider>
+      </body>
     </html>
   );
 }
-
