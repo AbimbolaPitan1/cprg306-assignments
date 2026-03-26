3e3e8a99b75a9a09b391f17bb138d60c4cddb63d
AbimbolaPitan1 <toluabimbola.pitan@gmail.com>
2026-03-26 10:59:50 -0600

    week-10

--- app/week-8/_utils/auth-context.tsx
+++ app/week-8/_utils/auth-context.tsx
@@ -7,7 +7,7 @@ import {
   onAuthStateChanged,
   GithubAuthProvider,
 } from "firebase/auth";
-import { auth } from "./firebase";
+import { auth } from "@/app/_utils/firebase";;
 
 const AuthContext = createContext<any>(null);
 
