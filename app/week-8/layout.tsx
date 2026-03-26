fc33648cbc72a0bde384feb5df4fb5518419bd3d
AbimbolaPitan1 <your-email-used-on-github>
2026-03-26 11:59:12 -0600

    changes

--- app/week-8/layout.tsx
+++ app/week-8/layout.tsx
@@ -1,4 +1,4 @@
-import { AuthContextProvider } from "./_utils/auth-context";
+import { AuthContextProvider } from "../week-10/_utils/auth-context";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <AuthContextProvider>{children}</AuthContextProvider>;
